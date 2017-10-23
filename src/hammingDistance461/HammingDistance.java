43e65652913b7b2cc98b4bd08528fdb2d3bde0f2
Jon Mercer <mercer.33@wright.edu>
2017-10-23 13:54:09 -0400

    Hamming Distance correct, but inefficient. Correct solution is 1 line.

--- src/hammingDistance461/HammingDistance.java
+++ src/hammingDistance461/HammingDistance.java
@@ -1,26 +1,24 @@
 package hammingDistance461;
 
 public class HammingDistance {
-	public static void output(int x, int y) {
-		System.out.println(2^3);
-	}
+
 	public static int hammingDistance(int x, int y) {
-        int hamValue = 0;
-        int bitValue = 1;
-        do {
-        	bitValue *= 2;
-            if ((x % bitValue == 0) != (y % bitValue == 0)){
-                hamValue++;
-            }
-            
-            
-        } while (x >= bitValue || y >= bitValue);
-        return hamValue;
-    }
+		int hamValue = 0;
+		int bitValue = 1;
+		do {
+			if ((x & bitValue) != (y & bitValue)) {
+				hamValue++;
+			}
+			bitValue *= 2;
+
+		} while (x >= bitValue || y >= bitValue);
+		return hamValue;
+	}
+
 	public static void main(String[] args) {
-		output(1, 4);
-		output(0, 8);
-		output(0, 0);
-		output(235245234, 95476923);
+		System.out.println(hammingDistance(1, 4));
+		System.out.println(hammingDistance(0, 8));
+		System.out.println(hammingDistance(0, 0));
+		System.out.println(hammingDistance(235245234, 95476923));
 	}
 }
